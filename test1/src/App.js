4b1cf400a3bbcaadcb4109614706ee4661006a5b
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-12-30 09:56:36 +0530

    updated

--- test1/src/App.js
+++ test1/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, {useState} from 'react';
 
 function App() {
+  const [value, setCount] = useState(4);
+  const dec = () =>{
+    setCount(value-1);
+  }
+  const inc = () =>{
+    setCount(value+1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <button onClick={dec}>-</button>
+         <span>{value}</span>
+         <button onClick={inc}>+</button>
     </div>
   );
 }
